08e0b88275b7f3fc4edb54df5a65d67a2375929c
Ada <139880994+Adamay01@users.noreply.github.com>
2023-11-17 22:08:18 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,30 @@
+
+    }
+    for (let i = 0; i < 10; i++) {
+    const data = generateRandomData();
+    return data;
+}
+main();
+    const data = [];
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+function generateRandomData() {
+
+    data.forEach(item => {
+    });
+        console.log("Random Number: " + item);
+function main() {
+}    main()
+if __name__ == "__main__":
+
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+    return data
+
+        print(f"Random Number: {item}")
+
+
+def main():
+    for item in data:
+import random
+    data = generate_random_data()
