8353480d092c5f4e0e07f20b0fb053df8659521f
Ada <139880994+Adamay01@users.noreply.github.com>
2023-11-17 23:39:37 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,66 @@
+
+        print(f"Random Number: {item}")
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+    for item in data:
+def generate_random_data():
+    main()
+def main():
+    data = generate_random_data()
+
+
+
+if __name__ == "__main__":
+
+if __name__ == "__main__":
+def main():
+    for item in data:
+    main()
+
+
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+def generate_random_data():
+import random    data = generate_random_data()
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+def main():
+
+    for item in data:
+
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+def generate_random_data():
+
+
+import randomdef main():
+    for item in data:
+    return data
+
+if __name__ == "__main__":
+    data = generate_random_data()
+
+    main()
+def generate_random_data():
+
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+
+import random
+
+        print(f"Random Number: {item}")
+
+def main():
+
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+    main()
+if __name__ == "__main__":
+import random
+    return data
+    for item in data:
